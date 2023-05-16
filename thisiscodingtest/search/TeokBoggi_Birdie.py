5496c4091da6ad998d9ea593702a6d4bfb26af83
birdieHyun <flaehdan@gmail.com>
2023-05-17 08:01:25 +0900

    이코테 떡길이찾기 버디[3]

---
+++ thisiscodingtest/search/TeokBoggi_Birdie.py
@@ -0,0 +1,25 @@
+counts, teok_length = map(int, input().split())
+
+teok_list = list(map(int, input().split()))
+
+answer = 0
+
+start = 0
+end = max(teok_list)
+
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+
+    for x in teok_list:
+        if x > mid:
+            total += x - mid
+
+    if total < teok_length:
+        end = mid -1
+
+    else:
+        answer = mid
+        start = mid + 1
+
+print(answer)
