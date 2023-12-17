f1aaa95d525c7a57a51a964931189d6b95e47aa8
lvalentine6 <ysr1227@naver.com>
2023-12-17 23:06:09 +0900

    feat: 프로그래머스 유사 칸토어 비트열

--- programmers/lv1/148652_roy.py
+++ programmers/lv1/148652_roy.py
@@ -1,9 +1,10 @@
 def solution(n, l, r):
     answer = 0
-    for l in range(l-1, r):
+    for l in range(l - 1, r):
         if is_one(l):
             answer += 1
     return answer
+
 
 def is_one(l):
     while l >= 5:
