407c62afadac385efd5ccab968fb85f2fe6bb78e
dokkisan <dokkisan.wang@gmail.com>
2023-12-23 05:07:22 +0900

    프로그래머스 개인정보 수집 유효기간(완성)

--- programmers/lv1/150370_Hana.py
+++ programmers/lv1/150370_Hana.py
@@ -1,40 +1,25 @@
 def solution(today, terms, privacies):
     answer = []
 
-    int_today = int(today.replace('.', ''))
+    this_year = int(today.split('.')[0]) * (12 * 28)
+    this_month = int(today.split('.')[1]) * 28
+    this_date = int(today.split('.')[2]) + this_year + this_month
 
-    parsed_terms = dict()
-    for i in terms:
-        parsed_terms[i.split(' ')[0]] = i.split(' ')[1]
+    terms_dic = dict()
+    for term in terms:
+        terms_dic[term.split(' ')[0]] = term.split(' ')[1]
 
     for index, privacy in enumerate(privacies):
-        stored_period = int(parsed_terms.get(privacy.split(' ')[1]))
-        year = int(privacy.split(' ')[0].split('.')[0])
-        month = int(privacy.split(' ')[0].split('.')[1])
-        day = int(privacy.split(' ')[0].split('.')[2])
-        category = privacy.split(' ')[1]
-        # print(month + stored_period)
-        if month + stored_period > 12:
-            changed_years = (month + stored_period) // 12
-            year = year + 1 * changed_years
+        year = int(privacy.split('.')[0])
+        month = int(privacy.split('.')[1])
+        day = int(privacy.split('.')[2].split(' ')[0])
 
-        day = (day + stored_period * 28)
-        for _ in range(day // 28):
-            day -= 28
-        day -= 1
-        if day == 0:
-            day = 28
-        elif day < 0:
-            day = 28 + day
+        year = year * (12 * 28)
+        month = (month + int(terms_dic[privacy.split(' ')[1]])) * 28
+        day = year + month + day
 
-        month = month + stored_period
-        if month > 12:
-            for _ in range(month // 12):
-                month -= 12
-
-        str_delete_day = str(year) + str(month).zfill(2) + str(day).zfill(2)
-        print(str_delete_day, int_today)
-        if int(str_delete_day) < int_today:
+        print(day, this_date)
+        if day <= this_date:
             answer.append(index + 1)
 
     return answer
