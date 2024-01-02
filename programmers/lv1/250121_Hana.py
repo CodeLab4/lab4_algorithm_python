775b74c2bd9871ea5cee691f400fe24e978e567f
dokkisan <dokkisan.wang@gmail.com>
2024-01-02 18:19:39 +0900

    프로그래머스 PCCE 기출 10번 ver.2 람다 변수명 수정

--- programmers/lv1/250121_Hana.py
+++ programmers/lv1/250121_Hana.py
@@ -1,8 +1,7 @@
 def solution(data, ext, val_ext, sort_by):
-    answer = [[]]
 
     condition_dic = ['code', 'date', 'maximum', 'remain']
     answer = [item for item in data if item[condition_dic.index(ext)] < val_ext]
-    answer = sorted(answer, key=lambda answer: answer[condition_dic.index(sort_by)])
+    answer = sorted(answer, key=lambda x: x[condition_dic.index(sort_by)])
 
     return answer
