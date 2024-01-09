9760874636432db7f10ac82f910161265776907c
dokkisan <dokkisan.wang@gmail.com>
2024-01-09 14:24:38 +0900

    프로그래머스 카페트 코드 수정

--- programmers/lv2/42842_Hana.py
+++ programmers/lv2/42842_Hana.py
@@ -7,14 +7,10 @@ def solution(brown, yellow):
         if total % i == 0:
             divisors.append(i)
 
-    if len(divisors) == 3:
-        answer = [divisors[1] for _ in range(2)]
-
-    else:
-        for i in range(len(divisors)):
-            for j in range(i, len(divisors)):
-                if divisors[i] * divisors[j] == total and (divisors[i] - 2) * (divisors[j] - 2) == yellow:
-                    answer.append(divisors[j])
-                    answer.append(divisors[i])
+    for i in range(len(divisors)):
+        for j in range(i, len(divisors)):
+            if divisors[i] * divisors[j] == total and (divisors[i] - 2) * (divisors[j] - 2) == yellow:
+                answer.append(divisors[j])
+                answer.append(divisors[i])
 
     return answer
