360052561f523f25d1507b019a9b85b7a2ad27c8
lvalentine6 <ysr1227@naver.com>
2024-01-15 18:24:48 +0900

    프로그래머스 H-index ver2

--- programmers/lv2/42747_roy.py
+++ programmers/lv2/42747_roy.py
@@ -3,17 +3,8 @@ def solution(citations):
 
     citations.sort(reverse=True)
 
-    h = len(citations)
-    flag = False
-
-    while not flag:
-        cnt = 0
-        for i in citations:
-            if i >= h:
-                cnt += 1
-        if cnt >= h:
-            flag = True
-            answer = h
-        h -= 1
+    for i in citations:
+        if i > answer:
+            answer += 1
 
     return answer
