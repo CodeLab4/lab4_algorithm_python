c2bb2d08873ddc2b7acf789ee37878f2f6663ed8
lvalentine6 <ysr1227@naver.com>
2024-01-23 03:32:48 +0900

    프로그래머스 코드 정렬

--- programmers/lv1/250121_Hana.py
+++ programmers/lv1/250121_Hana.py
@@ -1,5 +1,4 @@
 def solution(data, ext, val_ext, sort_by):
-
     condition_dic = ['code', 'date', 'maximum', 'remain']
     answer = [item for item in data if item[condition_dic.index(ext)] < val_ext]
     answer = sorted(answer, key=lambda x: x[condition_dic.index(sort_by)])
