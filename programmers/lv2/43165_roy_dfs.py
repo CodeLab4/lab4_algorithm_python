c4c093081ac4da5c943df19a1e607a235a977907
lvalentine6 <ysr1227@naver.com>
2024-02-06 14:15:40 +0900

    프로그래머스 타겟 넘버 dfs ver2

--- programmers/lv2/43165_roy_dfs.py
+++ programmers/lv2/43165_roy_dfs.py
@@ -1,21 +1,8 @@
 def solution(numbers, target):
-    answer = dfs_counter(numbers, target, 0, 0)
+    def dfs(index=0, current_sum=0):
+        if index == len(numbers):
+            return 1 if current_sum == target else 0
 
-    return answer
+        return dfs(index + 1, current_sum + numbers[index]) + dfs(index + 1, current_sum - numbers[index])
 
-
-def dfs_counter(numbers, target, num_sum, depth):
-    cnt = 0
-
-    def dfs(numbers, target, num_sum, depth):
-        nonlocal cnt
-        if depth == len(numbers):
-            if num_sum == target:
-                cnt += 1
-            return
-
-        dfs(numbers, target, num_sum + numbers[depth], depth + 1)
-        dfs(numbers, target, num_sum - numbers[depth], depth + 1)
-
-    dfs(numbers, target, num_sum, depth)
-    return cnt
+    return dfs()
