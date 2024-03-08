8d7e5e37f8e028c155a22d5c25122d02c3278641
lvalentine6 <ysr1227@naver.com>
2024-03-08 23:04:57 +0900

    프로그래머스 석유 시추 bfs

--- programmers/lv2/250136_roy.py
+++ programmers/lv2/250136_roy.py
@@ -3,31 +3,36 @@ from collections import deque
 
 def solution(land):
     answer = 0
-
     n, m = len(land), len(land[0])
+    visited = [[False] * m for _ in range(n)]
+    oil = [0] * m
     directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-    result = []
-    for i in range(m):
-        visited = [[False] * m for _ in range(n)]
-        cnt = 0
-        for j in range(n):
-            if land[j][i] == 1 and not visited[j][i]:
-                queue = deque()
-                queue.append((j, i))
-                visited[j][i] = True
-                cnt += 1
 
-                while queue:
-                    row, col = queue.popleft()
-                    for dr, dc in directions:
-                        nr = row + dr
-                        nc = col + dc
-                        if 0 <= nr < n and 0 <= nc < m and land[nr][nc] == 1 and not visited[nr][nc]:
-                            queue.append((nr, nc))
-                            visited[nr][nc] = True
-                            cnt += 1
+    def bfs(row, col):
+        queue = deque()
+        queue.append([row, col])
+        visited[row][col] = True
+        cnt = 1
+        oil_covered = {col}
 
-        result.append(cnt)
+        while queue:
+            pr, pc = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = pr + dr, pc + dc
+                if 0 <= nr < n and 0 <= nc < m and land[nr][nc] == 1 and not visited[nr][nc]:
+                    queue.append([nr, nc])
+                    visited[nr][nc] = True
+                    cnt += 1
+                    oil_covered.add(nc)
 
-    answer = max(result)
+        for c in oil_covered:
+            oil[c] += cnt
+
+    for i in range(n):
+        for j in range(m):
+            if land[i][j] == 1 and not visited[i][j]:
+                bfs(i, j)
+
+    answer = max(oil)
+
     return answer
