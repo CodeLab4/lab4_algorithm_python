86e4293db543c45abce765c902c9164727aba927
lvalentine6 <ysr1227@naver.com>
2024-03-15 19:41:01 +0900

    프로그래머스 오픈채팅방 수정

--- programmers/lv2/42888_roy.py
+++ programmers/lv2/42888_roy.py
@@ -3,24 +3,22 @@ from collections import defaultdict
 
 def solution(record):
     answer = []
-
     nick_dic = defaultdict(str)
+
     status_lst = []
-
     for m in record:
-        temp = m.split()
-        status_lst.append((temp[0], temp[1]))
+        parts = m.split()
+        action, user_id = parts[0], parts[1]
+        if action in ['Enter', 'Change']:
+            nick_dic[user_id] = parts[2]
+        if action != 'Change':
+            status_lst.append((action, user_id))
 
-        if len(temp) == 3:
-            nick_dic[temp[1]] = (temp[2])
-
-    for sl in status_lst:
-
-        if sl[0] == 'Enter':
-            message = nick_dic[sl[1]] + '님이 ' + '들어왔습니다.'
-            answer.append(message)
-        elif sl[0] == 'Leave':
-            message = nick_dic[sl[1]] + '님이 ' + '나갔습니다.'
-            answer.append(message)
+    for action, user_id in status_lst:
+        if action == 'Enter':
+            message = f'{nick_dic[user_id]}님이 들어왔습니다.'
+        elif action == 'Leave':
+            message = f'{nick_dic[user_id]}님이 나갔습니다.'
+        answer.append(message)
 
     return answer
