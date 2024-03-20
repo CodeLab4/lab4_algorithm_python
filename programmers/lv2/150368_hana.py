7406e36a53231e023fb5e97f82ab4a753a56c9f8
dokkisan <dokkisan.wang@gmail.com>
2024-03-20 23:59:11 +0900

    프로그래머스 이모티콘 할인행사

--- programmers/lv2/150368_hana.py
+++ programmers/lv2/150368_hana.py
@@ -1,24 +1,35 @@
-import itertools
+from itertools import *
 
 
 def solution(users, emoticons):
-    # 1. 가입자 최대한 늘리기
-    # 2. 판매액 최대한 늘리기
-
-    # 행사 방식
-    # n명의 사용자에게 이모티콘 m개를 할인해 판매
-    # 할인율은 이모티콘마다 다를 수 있고, 10,20,30,40% 중 하나로 선택
-
-    # 사용자 기준
-    # 각 사용자는 자신의 기준에 따라 일정 비율 이상 할인하는 이모티콘 모두 구매
-    # 자신의 기준에 따라 이모티콘 구매 비용의 합이 일정 가격 이상 -> 이모티콘 구매 취소, 플러스 가입
-
-    rate = [0.1, 0.2, 0.3, 0.4]
-
-    combinations = itertools.product(rate, emoticons)
-    for comb in combinations:
-        price = comb[1] - (comb[1] * comb[0])
-
     answer = []
+    rate = [0.4, 0.3, 0.2, 0.1]
+    comb = []
+    for rate_comb in product(rate, repeat=len(emoticons)):
+        temp_lst = []
+        for i in range(len(emoticons)):
+            temp_lst.append([rate_comb[i], emoticons[i] - emoticons[i] * rate_comb[i]])
+        comb.append(temp_lst)
+    for discount_lst in comb:
+        sum = 0
+        join_count = 0
+        for user in users:
+            pay = 0
+            for idx in range(len(emoticons)):
+                if user[0] <= discount_lst[idx][0] * 100:
+                    pay += discount_lst[idx][1]
+            if pay >= user[1]:
+                join_count += 1
+            else:
+                sum += pay
+
+        answer.append([join_count, int(sum)])
+
+    answer.sort(reverse=True)
+    answer = answer[0]
 
     return answer
+
+
+if __name__ == '__main__':
+    solution([[40, 10000], [25, 10000]], [7000, 9000])
